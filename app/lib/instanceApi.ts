a9f38ba89e17a7ca01b9289d8014ce974d46c232
Murali Kamme <Murali.k@Seanergy.ai>
2026-04-17 15:58:31 +0530

    changes

--- app/lib/instanceApi.ts
+++ app/lib/instanceApi.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 import toast from "react-hot-toast"
 
 export const api = axios.create({
-  baseURL: process.env.BASE_URL ,
+  baseURL: "/api",
   withCredentials: true
 })
 
